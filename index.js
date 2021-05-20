dea53d0c4335d4b3046252cf4b7c6811160b54f3
Danny Rivera <samboard@hotmail.com>
2021-05-20 19:41:41 -0400

    commit tree 2f10990ba71f6818a7d2ae248f2d64fb8fc1796a

---
+++ index.js
@@ -0,0 +1,8 @@
+// Has a functional expression called divide:
+let divide = () => 2000/100
+
+// Has an arrow function called square:
+let square = x => x * x
+
+// Has an arrow function called add:
+let add = (a,b) => a + b
